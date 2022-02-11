0184aefb4a55644367b30ef28eb7b1beff83d7c2
mohammad <mohammad.daaboul.99@gmail.com>
2022-02-11 22:53:30 +0200

    fix margin and display btn-close

--- src/App.js
+++ src/App.js
@@ -9,21 +9,29 @@ const App = () => {
     { id: 1, text: 'Functional App' },
     { id: 2, text: 'Landing Page' },
   ])
+  const[btnState , setState] = useState(false);
+
   const addNewTaskHandler = (newTask) => {
     setTasks((prevTasks) => {
       return prevTasks.concat(newTask);
     });
   }
-  let classess = ['btn','btn-close']
 
-  console.log(classess);
+  const bntShow = () =>{
+    setState(true)
+  }
+  const btnDisble = () =>{
+    setState(false)
+  }
+
+
   return (
     <React.Fragment>
       <Header />
       <section className='task'>
         <div className='btn-contaner'>
-          <a href='#Task' className='btn'>+</a>
-          <a href='#' className='btn'>-</a>
+          <a href='#Task' className='btn' onClick={bntShow}>+</a>
+          {btnState ? <a href='#'  className='btn' onClick={btnDisble}>-</a> : null }
         </div>
         <div className='Task-box' id='Task'>
           <NewTask className='task-input' addTask={addNewTaskHandler} />
